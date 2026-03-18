1dc5b5940cddb816fa6b5ba2abbbdabd6d1aac64
unknown <08250270.sce@rub.edu.bt>
2026-03-18 09:59:41 +0600

    uploading example of string

---
+++ Demo/stringEX.py
@@ -0,0 +1,12 @@
+print("Hello")
+print("World")
+multiplelines = """This is a string 
+that spans multiple lines."""
+print(multiplelines)
+
+a=1
+b=2
+c=a+b
+print(type(c))#Data Type of c is int
+c=str(c)# Type casting c to string
+print(type(c))#Data Type of c is now str 
