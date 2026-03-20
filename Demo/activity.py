6fe517515a1c9415aaf1fb6176af32895711074d
unknown <08250270.sce@rub.edu.bt>
2026-03-20 16:14:56 +0600

    activity

---
+++ Demo/activity.py
@@ -0,0 +1,16 @@
+name=input("Enter your name: ")
+print("Kuzuzangpo la "+name)
+
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+
+addition_result=num1+num2
+subtraction_result=num1-num2
+multiplication_result=num1*num2
+division_result=num1/num2
+
+print("Addition:",addition_result)
+print("Subtraction:",subtraction_result)
+print("multiplication:",multiplication_result)
+print("division:",division_result)
+
