062b205d4a55247832251cb4b0f5f474634d0fd7
unknown <08250270.sce@rub.edu.bt>
2026-03-20 15:41:04 +0600

    logical

---
+++ Demo/logicalEx.py
@@ -0,0 +1,10 @@
+x=True 
+y=False
+result_and=x and y #False (both x and y are not true)
+print("Logical AND:",result_and)
+
+result_or= x or y# True(x is true
+print("Logical OR:",result_or)
+
+result_not= not x #False (x is true, so not x is false)
+print("Logical NOT:",result_not)
