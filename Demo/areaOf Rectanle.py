dd0e7e35f9843d99bf9b78d75cb678bfcadb0f1c
unknown <08250270.sce@rub.edu.bt>
2026-03-20 15:32:47 +0600

    area of rectanle

--- Demo/areaOf Rectanle.py
+++ Demo/areaOf Rectanle.py
@@ -2,8 +2,6 @@ L=float(input("Enter the length of the rectangle: "))
 B=float(input("Enter the breadth of the rectangle: "))
 area=L*B
 print("The area of the rectangle is:",area)
-print(type(L))
-print(type(B))
-print(type(area))
+print("The area of the rectangle is: "+str(area))#Type casting area to string to concatenate with other string
 
 
