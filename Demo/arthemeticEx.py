4eee1aa18cf2445709e5fad4f9ad35ace1445972
unknown <08250270.sce@rub.edu.bt>
2026-03-20 15:06:04 +0600

    learning about arthemetic

---
+++ Demo/arthemeticEx.py
@@ -0,0 +1,13 @@
+a=10
+b=5
+addition_result=a+b#10+5=15
+subtraction_result=a-b#10-5=5
+multiplication_result=a*b#10*5=50
+division_result=a/b#10/5=2.0 (devisionalways returns a float)
+exponentiation_result=a**b#10**5=100000
+modulus_result=a%b#10%5=0 (remainder of division
+print("Addition:",addition_result)
+print("Subtraction:",subtraction_result)
+print("Multiplication:",multiplication_result)
+print("exponentiation:",exponentiation_result)
+print("modulus:",modulus_result)
