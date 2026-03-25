938e731e2d59483b213fa3eccdaf40134d066bae
unknown <08250270.sce@rub.edu.bt>
2026-03-25 09:59:09 +0600

    operator

---
+++ Demo/Oerator.py
@@ -0,0 +1,5 @@
+print("Currently,we are having %s class"%'PRG101')# Output: Currently, we are having PRG101 class
+
+subject='Python'
+module_no=101
+print("The module number of %s is %d" %(subject, module_no))# Output: The module number of pythone is 101
