8eb7dfbec0c7c8f39bca27511f36aacfdb7ea41a
unknown <08250270.sce@rub.edu.bt>
2026-03-26 15:57:52 +0600

    f-String

---
+++ Demo/f-string.py
@@ -0,0 +1,4 @@
+topic="formating Strings"
+print(f"We are currently learning {topic}")# Output: We are currently learning formating Strings
+
+print(f"There are 12 girls and 16boys in our class. So in total we have {12+16} students in our class.")# Output: There are 12 girls and 16boys in our class. So in total we have 28 students in our class.
